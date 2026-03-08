538c409f907cfb23df8a00f07a8820c5b8956a69
PJ <ipriyanshujain@gmail.com>
2026-03-09 00:46:23 +0700

    refactor(daemon): split file locking by platform

---
+++ daemon/lock_windows.go
@@ -0,0 +1,58 @@
+//go:build windows
+
+package daemon
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+	"unsafe"
+)
+
+var (
+	modkernel32    = syscall.NewLazyDLL("kernel32.dll")
+	procLockFileEx = modkernel32.NewProc("LockFileEx")
+	procUnlockFile = modkernel32.NewProc("UnlockFileEx")
+)
+
+const (
+	lockfileExclusiveLock   = 0x00000002
+	lockfileFailImmediately = 0x00000001
+)
+
+// acquireLock takes an exclusive file lock to prevent multiple daemon instances.
+// Returns the lock file which must be kept open for the lifetime of the daemon.
+func acquireLock() (*os.File, error) {
+	f, err := os.OpenFile(lockPath(), os.O_CREATE|os.O_RDWR, 0600)
+	if err != nil {
+		return nil, fmt.Errorf("open lock file: %w", err)
+	}
+
+	ol := new(syscall.Overlapped)
+	r1, _, _ := procLockFileEx.Call(
+		uintptr(f.Fd()),
+		uintptr(lockfileExclusiveLock|lockfileFailImmediately),
+		0,
+		1, 0,
+		uintptr(unsafe.Pointer(ol)),
+	)
+	if r1 == 0 {
+		f.Close()
+		return nil, fmt.Errorf("daemon is already running")
+	}
+
+	return f, nil
+}
+
+// releaseLock releases the file lock and removes the lock file.
+func releaseLock(f *os.File) {
+	ol := new(syscall.Overlapped)
+	procUnlockFile.Call(
+		uintptr(f.Fd()),
+		0,
+		1, 0,
+		uintptr(unsafe.Pointer(ol)),
+	)
+	f.Close()
+	os.Remove(lockPath())
+}
